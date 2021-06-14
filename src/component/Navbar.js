cfbe9cade5b81e9eca02edcbb71af50279b221a7
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-06-14 00:25:09 -0300

    menu desplegable lateral listo

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import './styles/Navbar.css'
 import Logo from '../assets/Logo/Simbolo.svg'
+import CloseLogo from '../assets/Icons/uil_plus-closed.svg'
 function Navbar() {
     return (
         <nav>
             <figure className="logo">
-                <img className src={Logo} alt="" />
+                <img src={Logo} alt="logo" />
             </figure>
             <button className="btn burger-menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
             </button>
 
             <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                <div className="offcanvas-header">
-                    <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <div className="offcanvas-header offcanvas_header">
+                    <img src={Logo} alt="logo" />
+                    <button type="button" className=" btn_close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"><img className="btn_close--icon" src={CloseLogo} /></button>
                 </div>
-                <div className="offcanvas-body">
-                    ...
+                <div className="offcanvas_body offcanvas-body">
+                    <ul>
+                        <li>Home</li>
+                        <li>Recomendados</li>
+                        <li>La agencia</li>
+                        <li>Contacto</li>
+                    </ul>
                 </div>
             </div>
         </nav>
